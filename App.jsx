1df3c68a52ccb6c006a2c9bcade30848f2187102
Javier Cortes Armas <71993977+JavierCortesArmas@users.noreply.github.com>
2023-01-21 23:17:39 -0600

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import VENTANA1 from './Ventana1';
+export default function App() {
+  return (
+    <VENTANA1 />
+  );
+}
+
+
